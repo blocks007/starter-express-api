5facd5ad69ef5ab196c82464ed25228e6adf821b
blocks007 <123449214+blocks007@users.noreply.github.com>
2023-01-24 13:41:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+app.get('/home', (req, res) => {
+    console.log("Just got a request!")
+    res.send('home!')
+})
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
